382d9c70b30aa336abe25518be45dc2559773630
Prince Roshan <princekrroshan01@gmail.com>
2021-06-25 10:27:37 +0530

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('input, select').on('focus', function () {
+        $(this).parent().find('.input-group-text').css('border-color', '#80bdff');
+    });
+    $('input, select').on('blur', function () {
+        $(this).parent().find('.input-group-text').css('border-color', '#ced4da');
+    });
+});
+
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
